897ae38d22850f8e4ed613cc6843fb505c8500f7
nero7303 <bin1018119@gmail.com>
2025-05-18 02:42:48 +0900

    first commit

--- script/script.js
+++ script/script.js
@@ -12,21 +12,29 @@ header.classList.remove("on");
 };
 });
 
-
-
-
-
-// Parallax 메뉴 이동
-let menu = document.querySelectorAll("#header ul>li>a");
-let section = document.querySelectorAll("body > section");
-menu.forEach(navList => navList.addEventListener('click',function (e) {
-  e.preventDefault();
-  document.querySelector(navList.getAttribute('href')).scrollIntoView({
- behavior: 'smooth'
-  })
-}));
 };
+
+
+$(function () {
+  let menu = $("#header ul>li");
+ let section = $("body > section");
+
+ menu.on('click',function (event) {
+event.preventDefault();
+  let index = $(this).index();
+ let nav = $(section).eq(index).offset().top;
+ $(menu).removeClass("active");
+$(this).addClass("active");
+
+
+ $("html ,body").animate({scrollTop : nav - 100},600);
+
+ });
+ 
   
+});
+
+
 
 
 
@@ -40,24 +48,6 @@ menu.forEach(navList => navList.addEventListener('click',function (e) {
 
 
     
-// $(function () {
-//   let menu = $("#header ul>li");
-//  let section = $("body > section");
-
-//  menu.on('click',function (event) {
-// event.preventDefault();
-//   let index = $(this).index();
-//  let nav = $(section).eq(index).offset().top;
-//  $(menu).removeClass("active");
-// $(this).addClass("active");
-
-
-//  $("html ,body").animate({scrollTop : nav - 100},600);
-
-//  });
- 
-  
-// });
   
 
 
